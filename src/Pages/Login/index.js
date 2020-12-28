c7cbe21f1fa49bc153a892b7a3ec88958c1fdfaf
larkloss <dingzhao1995@gmail.com>
2020-12-28 15:10:23 +0800

    6-3-3 return problem

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { Tabs, Form } from "antd";
-import { UserOutlined, LockTwoTone } from "@ant-design/icons";
+import React, {useState} from "react";
+import {Link} from'react-router-dom';
+import { Tabs, Form, Checkbox, Row } from "antd";
+import { UserOutlined, LockTwoTone,MobileTwoTone, MailTwoTone } from "@ant-design/icons";
 import styles from "./index.module.less";
 import InputItem from "../../components/InputItem";
 import SubmitButton from "../../components/SubmitButton";
@@ -8,6 +9,7 @@ import SubmitButton from "../../components/SubmitButton";
 const { TabPane } = Tabs;
 
 const Login = () => {
+    const [autoLogin, setAutoLogin] = useState(true);
     const [form] = Form.useForm();
     const handleFinish = (values) => {
         console.log(values);
@@ -34,9 +36,41 @@ const Login = () => {
                             size="large"
                         />
                     </TabPane>
-                    <TabPane tab="手机号登陆" key="2"></TabPane>
+                    <TabPane tab="手机号登陆" key="1">
+                        <InputItem
+                            name="mobile"
+                            prefix={<MobileTwoTone styles={{ color: "#1890ff" }} />}
+                            placeholder="手机号"
+                            size="large"
+                            rules={[{
+                                required: true,
+                                message: "plz input"
+                            }]}
+                        />
+                        <InputItem
+                            name="captcha"
+                            prefix={<MailTwoTone />}
+                            placeholder="验证码"
+                            size="large"
+                            rules={[{
+                                required: true,
+                                message: "plz input"
+                            }]}
+                        />
+                    </TabPane>
                 </Tabs>
+                <Row justify={"space-between"}>
+                    <Checkbox
+                        checked={autoLogin}
+                        onChange={(e)=>setAutoLogin(e.target.checked)}>
+                        自动登录
+                    </Checkbox>
+                    <a href={"#!"}>忘记密码</a>
+                    </Row>
                 <SubmitButton>Login</SubmitButton>
+                <div>
+                    <Link className={styles.register} to={"/register"}>注册账户</Link>
+                    </div>
             </div>
         </div>
     );
--- src/components/InputItem/index.js
+++ src/components/InputItem/index.js
@@ -1,19 +1,57 @@
-import React from "react";
-import { Input, Form, Button } from "antd";
+import React , {useState, useEffect} from "react";
+import { Input, Form, Button, Row, Col, message } from "antd";
+import styles from'./index.module.less'
 
 const InputItem = (props) => {
     const { name, rules, ...rest } = props;
-    return (
-        <Form.Item name={name} rules={rules}>
-            <Input {...rest} />;
+    const [timing, setTiming] = useState(false); //是否开始计时
+    const handleClickCaptcha = () => {
+        message.success('get message successful');
+        setTiming(true);
+    }
+    const [count, setCount] = useState(props.countDown || 60);
+    useEffect( () => {
+            let interval = 0;
+            if (timing) {
+                interval = window.setInterval(() => {
+                    setCount((preSecond) => {
+                        if (preSecond <= 1) {
+                            setTiming(false);
+                            //clearImmediateInterval(interval)
+                            return props.countDown || 60;
+                        }
+                        return preSecond - 1;
+                    })
+                }, 1000);
+            }
+            return () => clearInterval(interval)
+        }
+    ,[timing])
 
-        </Form.Item>
-    );
-    return (
-        <Form.Item name={name} rules={rules}>
-            <Input {...rest}/>
-        </Form.Item>
-    )
+    if (name === 'captcha') {
+        return (
+            <Form.Item name={name} rules={rules}>
+                <Row>
+                    <Col span={12}>
+                <Input {...rest} />;
+                        </Col>
+                    <Col span={6}>
+                <Button
+                    className={styles.getCaptcha}
+                    size={"large"}
+                    onClick={handleClickCaptcha}
+                >{timing ? '${count}秒' : '获取验证码'}
+                </Button>
+                </Col>
+                    </Row>
+            </Form.Item>
+        );
+        return (
+            <Form.Item name={name} rules={rules}>
+                <Input {...rest}/>
+            </Form.Item>
+        )
+    }
 };
 
 export default InputItem;
